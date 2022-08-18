f586b5bfcad950d33f8e5b804a5b6380b5a9409d
nqo0324 <niqinou@gmail.com>
2022-08-18 23:37:36 +0900

    详情页面的添加以及base模版的生成

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,13 +1,20 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
@@ -19,3 +26,9 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def index(request):
     post_list = Post.objects.all().order_by('created_time')
+    # 相当于是定义了一个 业务逻辑（logic）
+    # index这个视图函数（类比java的Servlet）需要根据创建时间倒序来显示所有文章标题
     return render(request, 'blog/index.html', context={'post_list': post_list})
+    # 类似于Servlet的forward
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # 定义文章详情，从URL中捕捉文章的id之后在数据库当中获取与之对应的Post数据（也就是文章）
+    # 这里用到了 get_object_or_404。其作用是：当传入的pk对应的Post在数据库存在时，return对应的post数据
+    # 如果不存在，就给用户返回一个404错误，已表明该文章不存在
+
+    return render(request, 'blog/detail.html', context={'post': post})
